63de89b6ce363e41464ae6cba428ffc3c2554d89
enbyio <59515841+enbyio@users.noreply.github.com>
2026-05-31 15:09:22 +0200

    extend error

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,30 @@ pub mod parsers;
 pub mod schema;
 pub mod store;
 
-use diesel::result::Error;
-
 #[derive(Debug)]
 pub enum StoreError {
     GeneralError(String),
     DatabaseError(String),
     DataError(String),
     ConnectionError(String),
+    MigrationError(String),
     UnsupportedInputData,
 }
 
-impl From<Error> for StoreError {
-    fn from(value: Error) -> Self {
+impl From<diesel::result::Error> for StoreError {
+    fn from(value: diesel::result::Error) -> Self {
         StoreError::DatabaseError(value.to_string())
+    }
+}
+
+impl From<diesel::r2d2::Error> for StoreError {
+    fn from(value: diesel::r2d2::Error) -> Self {
+        StoreError::ConnectionError(value.to_string())
+    }
+}
+
+impl From<diesel::r2d2::PoolError> for StoreError {
+    fn from(value: diesel::r2d2::PoolError) -> Self {
+        StoreError::ConnectionError(value.to_string())
     }
 }
