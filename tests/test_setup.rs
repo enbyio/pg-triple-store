524b78a579b67f9dc2f71b508ff31762c511282c
enbyio <59515841+enbyio@users.noreply.github.com>
2026-06-04 00:02:14 +0200

    update sparql parser module structure

--- tests/test_setup.rs
+++ tests/test_setup.rs
@@ -12,17 +12,16 @@ fn test_migrate() {
         .filter(None, log::LevelFilter::Info)
         .init();
     let mut store = TripleStore::new_from_env().unwrap();
-    assert!(store.reset_db().is_ok());
-    assert!(store
-        .import_turtle_from_url("https://schema.org/version/latest/schemaorg-current-https.ttl")
-        .is_ok());
+    // assert!(store.reset_db().is_ok());
+    // assert!(store
+    //     .import_turtle_from_url("https://schema.org/version/latest/schemaorg-current-https.ttl")
+    //     .is_ok());
     store
         .print_sparql_result(
             "SELECT ?s ?o
             WHERE {
-                ?s rdf:type ?o
-            }
-            LIMIT 10",
+                ?s rdfs:label ?o
+            }",
         )
         .unwrap();
 }
