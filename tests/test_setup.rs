1dc3d8ec75d52129cfdd77f422b85c6946333eec
enbyio <59515841+enbyio@users.noreply.github.com>
2026-06-19 12:21:42 +0200

    work in progress

--- tests/test_setup.rs
+++ tests/test_setup.rs
@@ -6,22 +6,22 @@ fn setup_db() {
     assert!(store.is_ok())
 }
 
-#[test]
-fn test_migrate() {
-    env_logger::Builder::from_default_env()
-        .filter(None, log::LevelFilter::Info)
-        .init();
-    let mut store = TripleStore::new_from_env().unwrap();
-    // assert!(store.reset_db().is_ok());
-    // assert!(store
-    //     .import_turtle_from_url("https://schema.org/version/latest/schemaorg-current-https.ttl")
-    //     .is_ok());
-    store
-        .print_sparql_result(
-            "SELECT ?s ?o
-            WHERE {
-                ?s rdfs:label ?o
-            }",
-        )
-        .unwrap();
-}
+// #[test]
+// fn test_migrate() {
+//     env_logger::Builder::from_default_env()
+//         .filter(None, log::LevelFilter::Info)
+//         .init();
+//     let mut store = TripleStore::new_from_env().unwrap();
+//     // assert!(store.reset_db().is_ok());
+//     // assert!(store
+//     //     .import_turtle_from_url("https://schema.org/version/latest/schemaorg-current-https.ttl")
+//     //     .is_ok());
+//     store
+//         .print_sparql_result(
+//             "SELECT ?s ?o
+//             WHERE {
+//                 ?s rdfs:label ?o
+//             }",
+//         )
+//         .unwrap();
+// }
