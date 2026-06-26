ce8e182e3451a69183e949727d57ea9f519c7f0e
enbyio <59515841+enbyio@users.noreply.github.com>
2026-06-26 13:27:56 +0200

    add sparql test

--- tests/test_setup.rs
+++ tests/test_setup.rs
@@ -6,6 +6,39 @@ fn setup_db() {
     assert!(store.is_ok())
 }
 
+#[test]
+fn load_test_turtle_file() {
+    env_logger::Builder::from_default_env()
+        .filter(None, log::LevelFilter::Info)
+        .init();
+    let mut store = TripleStore::new_from_env().unwrap();
+    assert!(store.reset_db().is_ok());
+    assert!(store.import_turtle_file("tests/test_data/test.ttl").is_ok());
+}
+
+#[test]
+fn test_triple_parsing() {
+    env_logger::Builder::from_default_env()
+        .filter(None, log::LevelFilter::Debug)
+        .init();
+    let mut store = TripleStore::new_from_env().unwrap();
+    assert!(store.reset_db().is_ok());
+    assert!(store.import_turtle_file("tests/test_data/test.ttl").is_ok());
+
+    // Case 1:
+    println!(
+        "{:?}",
+        store
+            .parse_sparql_query(
+                "SELECT ?friend ?name WHERE {
+      ex:alice ex:knows ?friend .
+      ?friend ex:name ?name .
+    }",
+            )
+            .unwrap()
+    );
+}
+
 // #[test]
 // fn test_migrate() {
 //     env_logger::Builder::from_default_env()
