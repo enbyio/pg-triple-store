0e3ddf283f651b82a18a951c2a305581603d3536
enbyio <59515841+enbyio@users.noreply.github.com>
2026-09-10 23:44:21 +0200

    schema updated for the new migrations

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,13 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    entities (id) {
+        id -> Int8,
+        entity_type -> Int2,
+    }
+}
+
+diesel::table! {
     objects (id) {
         id -> Int8,
         kind -> Text,
@@ -32,6 +39,25 @@ diesel::table! {
 }
 
 diesel::table! {
+    quoted_properties (id) {
+        id -> Int8,
+        subject -> Int8,
+        predicate -> Int8,
+        literal_value -> Text,
+        literal_type -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    quoted_relations (id) {
+        id -> Int8,
+        subject -> Int8,
+        predicate -> Int8,
+        object -> Int8,
+    }
+}
+
+diesel::table! {
     relations (subject, predicate, object) {
         subject -> Int8,
         predicate -> Int8,
@@ -39,8 +65,20 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(properties -> objects (subject));
+diesel::joinable!(objects -> entities (id));
+diesel::joinable!(properties -> entities (subject));
 diesel::joinable!(properties -> predicates (predicate));
+diesel::joinable!(quoted_properties -> predicates (predicate));
+diesel::joinable!(quoted_relations -> predicates (predicate));
 diesel::joinable!(relations -> predicates (predicate));
 
-diesel::allow_tables_to_appear_in_same_query!(objects, predicates, prefixes, properties, relations,);
+diesel::allow_tables_to_appear_in_same_query!(
+    entities,
+    objects,
+    predicates,
+    prefixes,
+    properties,
+    quoted_properties,
+    quoted_relations,
+    relations,
+);
